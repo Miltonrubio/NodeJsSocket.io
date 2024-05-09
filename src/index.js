8a593ceb553a3ddd2b55aa535be4571f8c8b8973
Miltonrubio <a3519110011@alumno.uttehuacan.edu.mx>
2024-05-09 16:02:06 -0600

    primer commit

--- src/index.js
+++ src/index.js
@@ -3,13 +3,88 @@ console.log("Conectado");
 import { Server as WebSocketServer } from "socket.io";
 import express from "express";
 import http from "http";
+import { log } from "console";
+
+
 const connection = require("./bdconfig");
+const session = require('express-session');
+
+
+
+
 
 const app = express();
 const httpServer = http.createServer(app);
 const io = new WebSocketServer(httpServer);
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+app.use(session({
+  secret: 'secreto',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000
+  }
+}));
+
+
+
+
+
+
+app.get('/cerrar-sesion', (req, res) => {
+  // Destruir la sesión
+  req.session.destroy(err => {
+    if (err) {
+      console.error(err);
+      res.send('Error al cerrar sesión');
+    } else {
+      res.send('Sesión cerrada');
+    }
+  });
+});
 
 app.use(express.static(__dirname + "/public"));
+
+
+app.post('/inicio', (req, res) => {
+  const { username } = req.body;
+  req.session.usuario = username;
+  res.redirect('/');
+});
+
+function validarSesion(req, res, next) {
+  if (!req.session.usuario || req.session.usuario != '') {
+
+    console.log("No hay sesion ")
+    return res.redirect('/login.html');
+  } else {
+    console.log("Si hay sesion ")
+    next();
+  }
+}
+
+
+
+
+// Rutas protegidas por la validación de sesión
+app.get('/', validarSesion, (req, res) => {
+  res.sendFile(__dirname + "/index2.html");
+
+});
+
+
+
+app.get('/api/sesion', (req, res) => {
+  res.json({ usuario: req.session.usuario });
+});
+
+
+
 
 app.use(express.text());
 app.use(express.json());
@@ -22,10 +97,10 @@ httpServer.listen(3000);
 
 io.on("connection", (socket) => {
   console.log("Nueva conexion");
-/*
- const mensajes = TomarMensajesDeBD();
-  io.sockets.emit("chat:message", mensajes);
-*/
+  /*
+   const mensajes = TomarMensajesDeBD();
+    io.sockets.emit("chat:message", mensajes);
+  */
 
 
   // Llama a la función para tomar los mensajes y maneja el resultado
@@ -33,7 +108,7 @@ io.on("connection", (socket) => {
     .then(mensajes => {
       // Haces lo que quieras con los mensajes, por ejemplo, guardarlos en una variable
       const mensajesGuardados = mensajes;
-    //  console.log("Mensajes guardados:", mensajesGuardados);
+      //  console.log("Mensajes guardados:", mensajesGuardados);
       io.sockets.emit("chat:message", mensajesGuardados);
     })
     .catch(error => {
@@ -49,9 +124,81 @@ io.on("connection", (socket) => {
     EnviarDatosABD(titulo, mensajeTomado, socket);
 
 
-    
   });
+
+  socket.on("client:editarMensaje", (data) => {
+    const titulo = data.titulo;
+    const mensaje = data.mensaje;
+    const id = data.id;
+
+    EditarMensaje(titulo, mensaje, id)
+  });
+
+
+
+
+  socket.on("client:eliminarMensaje", (data) => {
+    const id = data.id;
+
+    EliminarMensaje(id)
+  });
+
+
+
 });
+
+function EliminarMensaje(id) {
+  const query = "UPDATE mensajes SET status = 0 WHERE ID_mensaje = ?";
+  connection.query(query, [id], (error, results) => {
+    if (error) {
+      console.error("Error al ejecutar la consulta:", error);
+      return;
+    }
+    console.log("Se elimino el mensaje");
+    TomarMensajesDeBD()
+      .then(mensajes => {
+        // Haces lo que quieras con los mensajes, por ejemplo, guardarlos en una variable
+        const mensajesGuardados = mensajes;
+        //  console.log("Mensajes guardados:", mensajesGuardados);
+        io.sockets.emit("chat:message", mensajesGuardados);
+      })
+      .catch(error => {
+        // Maneja cualquier error que pueda ocurrir durante la consulta
+        console.error("Error al tomar los mensajes de la base de datos:", error);
+      });
+  });
+
+}
+
+
+
+
+
+function EditarMensaje(titulo, mensaje, id) {
+  const query = "UPDATE mensajes SET titulo = ?, cuerpo = ? WHERE ID_mensaje = ?";
+  connection.query(query, [titulo, mensaje, id], (error, results) => {
+    if (error) {
+      console.error("Error al ejecutar la consulta:", error);
+      return;
+    }
+    console.log("Se corrigio el mensaje");
+    TomarMensajesDeBD()
+      .then(mensajes => {
+        // Haces lo que quieras con los mensajes, por ejemplo, guardarlos en una variable
+        const mensajesGuardados = mensajes;
+        //  console.log("Mensajes guardados:", mensajesGuardados);
+        io.sockets.emit("chat:message", mensajesGuardados);
+      })
+      .catch(error => {
+        // Maneja cualquier error que pueda ocurrir durante la consulta
+        console.error("Error al tomar los mensajes de la base de datos:", error);
+      });
+  });
+
+}
+
+
+
 
 function EnviarDatosABD(titulo, mensajeTomado, socket) {
   const query = "INSERT INTO mensajes (titulo, cuerpo) VALUES (?, ?)";
@@ -69,7 +216,7 @@ function EnviarDatosABD(titulo, mensajeTomado, socket) {
     .then(mensajes => {
       // Haces lo que quieras con los mensajes, por ejemplo, guardarlos en una variable
       const mensajesGuardados = mensajes;
-    //  console.log("Mensajes guardados:", mensajesGuardados);
+      //  console.log("Mensajes guardados:", mensajesGuardados);
       io.sockets.emit("chat:message", mensajesGuardados);
     })
     .catch(error => {
@@ -77,24 +224,22 @@ function EnviarDatosABD(titulo, mensajeTomado, socket) {
       console.error("Error al tomar los mensajes de la base de datos:", error);
     });
 
-
-
 }
 
 function TomarMensajesDeBD() {
-    return new Promise((resolve, reject) => {
-      const query = "SELECT * FROM mensajes";
-  
-      connection.query(query, (error, results) => {
-        if (error) {
-          console.error("Error al ejecutar la consulta:", error);
-          reject(error);
-        } else {
-          resolve(results);
-        }
-      });
+  return new Promise((resolve, reject) => {
+    const query = "SELECT * FROM mensajes WHERE status = 1";
+
+    connection.query(query, (error, results) => {
+      if (error) {
+        console.error("Error al ejecutar la consulta:", error);
+        reject(error);
+      } else {
+        resolve(results);
+      }
     });
-  }
-  
+  });
+}
+
 
 console.log("Servidor en el puerto 3000");
--- src/public/main.js
+++ src/public/main.js
@@ -1,34 +1,123 @@
 const socket = io();
 
+
+
+fetch('/api/sesion')
+.then(response => response.json())
+.then(data => {
+    // Actualizar el contenido del elemento h1 con los datos de sesión
+    document.getElementById('tituloCRUD').textContent = `Usuario: ${data.usuario}`;
+})
+.catch(error => {
+    console.error('Error al obtener los datos de sesión:', error);
+});
+
+
+
+
 const noteForm = document.querySelector("#formNote");
 const tituloMensaje = document.querySelector("#title");
 const descrip = document.querySelector("#descrip");
 
 
+const contenedorNotas = document.getElementById("notes");
 
-const contenedorNotas =  document.getElementById("notes");
-
-socket.on('chat:message', function (mensajes){
-    console.log(mensajes); 
+socket.on('chat:message', function (mensajes) {
+    console.log(mensajes);
     contenedorNotas.innerHTML = ""
 
-    mensajes.forEach(function(mensaje) {
+    mensajes.forEach(function (mensaje) {
         contenedorNotas.innerHTML += `
-        <div class="card card-content mb-2 ">
+
+
+        <div class="card  p-2  card-content mb-2" >
+        <div class="row">
+        <div class=" col-9">
         <h4 class="titulo text-primary"> ${mensaje.titulo}</h4>
 
         <p>${mensaje.cuerpo} </p>
+        </div>
+        <div class=" col-3 my-auto">
+        <button class="btn btn-danger " type="button"  onclick="modalEliminar( ${mensaje.ID_mensaje},'${mensaje.titulo}');"> Eliminar </button>
+        <button class="btn btn-warning"  type="button"  onclick="modalEditar( ${mensaje.ID_mensaje},'${mensaje.titulo}', '${mensaje.cuerpo}' );"> Editar </button>
+        </div>
+        </div>
         </div>
         `;
     });
 });
 
 
+function modalEliminar (id, titulo){
+    $('#form_eliminar')[0].reset();
+    $('#modal_eliminar').modal('show');
+
+    $('#ID_elim').val(id);
+    $('#tituloMen').val(titulo);
+
+    $('#textoEliminacion').text("Confirmación para la liminacion del mensaje " +titulo);
+
+    
+}
+const formEliminar = document.querySelector("#form_eliminar");
+
+
+formEliminar.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const ID_elim = document.querySelector("#ID_elim");
+
+
+    socket.emit("client:eliminarMensaje", {
+        id: ID_elim.value,
+    })
+
+
+    $('#form_eliminar')[0].reset();
+
+    $('#modal_eliminar').modal('hide');
+
+});
 
 
 
 
+function modalEditar(id, titulo, cuerpo) {
 
+    $('#form_editar')[0].reset();
+    $('#modal_editar').modal('show');
+
+
+    $('#ID_editar').val(id);
+    $('#tituloMen').val(titulo);
+    $('#cuerpoMen').val(cuerpo);
+}
+
+const formEditar = document.querySelector("#form_editar");
+
+
+formEditar.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+
+
+    const tituloMensaje = document.querySelector("#tituloMen");
+    const descrip = document.querySelector("#cuerpoMen");
+    const ID_editar = document.querySelector("#ID_editar");
+
+
+    socket.emit("client:editarMensaje", {
+        id: ID_editar.value,
+        titulo: tituloMensaje.value,
+        mensaje: descrip.value,
+    })
+
+
+    $('#form_editar')[0].reset();
+
+    $('#modal_editar').modal('hide');
+
+});
 
 
 
@@ -37,23 +126,31 @@ socket.on('chat:message', function (mensajes){
 
 
 noteForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  console.log();
+    if (descrip.value === '' || tituloMensaje.value === '') {
 
-  socket.emit("client:newMessage", {
-   mensaje: descrip.value, 
-  titulo:  tituloMensaje.value
-  })
+        alert("No puedes tener campos vacios");
 
+    } else {
 
 
+        socket.emit("client:newMessage", {
+            mensaje: descrip.value,
+            titulo: tituloMensaje.value
+        })
+
+
+        tituloMensaje.value = '';
+        descrip.value = '';
+
+    }
 
 });
 
 
 
-socket.on ("MensajeSubido", () =>{
+socket.on("MensajeSubido", () => {
 
     console.log("Se subió el mensaje")
 
